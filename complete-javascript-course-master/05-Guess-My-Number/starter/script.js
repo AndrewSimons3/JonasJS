9eb9926e0032076f65f8055613d3076e7e7fc47a
AndrewSimons3 <simdrew07@gmail.com>
2021-09-20 21:04:08 -0500

    implemented reset feature

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -12,7 +12,18 @@ const secretNumber = Math.trunc(Math.random()*20)+1;
 
 let score = 20;
 
-document.querySelector('.number').textContent = secretNumber;
+document.querySelector('.again').addEventListener('click', () => {
+  score = 20;
+  const secretNumber = Math.trunc(Math.random()*20)+1;
+
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.message').textContent = "Start guessing..."
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('body').style.backgroundColor = '#222';
+})
 
 document.querySelector('.check').addEventListener('click', () => {
   const guess = Number(document.querySelector('.guess').value);
@@ -25,6 +36,7 @@ document.querySelector('.check').addEventListener('click', () => {
     //When Player wins
   } else if (guess === secretNumber)  {
     document.querySelector('.message').textContent = 'Correct Number!';
+    document.querySelector('.number').textContent = secretNumber;
 
     document.querySelector('body').style.backgroundColor = '#60b347';
 
