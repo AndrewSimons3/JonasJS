27def02ef7e16d04f015b559e5a713f9973ffcce
AndrewSimons3 <simdrew07@gmail.com>
2021-09-20 20:37:56 -0500

    updated styles when winning the game

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -17,20 +17,41 @@ document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', () => {
   const guess = Number(document.querySelector('.guess').value);
   console.log(typeof guess);
-
+  
+  //When there is no input
   if(!guess) {
     document.querySelector('.message').textContent = 'No Number!';
+
+    //When Player wins
   } else if (guess === secretNumber)  {
     document.querySelector('.message').textContent = 'Correct Number!';
-  } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = "Too high!";
-    score--;
-    document.querySelector('.score').textContent = score;
-  } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = "Too low!";
-    score--;
-    document.querySelector('.score').textContent = score;
-  }
 
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem'
+
+
+    //When guess is too high
+  } else if (guess > secretNumber) {
+    if(score > 1) {
+      document.querySelector('.message').textContent = "Too high!";
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = "You lost the game!"
+      document.querySelector('.score').textContent = 0;
+    }
+
+    //When guess is too low
+  } else if (guess < secretNumber) {
+    if(score > 1) {
+      document.querySelector('.message').textContent = "Too low!";
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = "You lost the game!"
+      document.querySelector('.score').textContent = 0;
+    }
+  }
 });
 
