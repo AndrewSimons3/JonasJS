0e8cf060db1b7773688251f14ae8f1f2c46c431f
AndrewSimons3 <simdrew07@gmail.com>
2021-09-21 06:58:53 -0500

    refactored code

--- complete-javascript-course-master/06-Modal/starter/script.js
+++ complete-javascript-course-master/06-Modal/starter/script.js
@@ -3,20 +3,24 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
 
-for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener('click', () => {
-    console.log("Button clicked")
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
-}
 
 const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 }
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
