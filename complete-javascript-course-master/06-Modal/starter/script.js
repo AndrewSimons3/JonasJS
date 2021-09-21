dd8a72b5fd5fb700190c2666f206287a8b6f8201
AndrewSimons3 <simdrew07@gmail.com>
2021-09-21 06:57:18 -0500

    close modal function created

--- complete-javascript-course-master/06-Modal/starter/script.js
+++ complete-javascript-course-master/06-Modal/starter/script.js
@@ -13,8 +13,11 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
   });
 }
 
-btnCloseModal.addEventListener('click', () => {
+const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
-})
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
 
