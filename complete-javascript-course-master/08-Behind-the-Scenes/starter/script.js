be9ed66318a44a3245d035ceb29d2340e0544bf1
AndrewSimons3 <simdrew07@gmail.com>
2021-09-28 07:56:06 -0500

    scope

--- complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
+++ complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
@@ -4,17 +4,31 @@ function calcAge(birthYear) {
   const age = 2037 - birthYear;
   
   function printAge() {
-    const output = `${firstName} You are ${age}, born in ${birthYear}`;
+    let output = `${firstName} You are ${age}, born in ${birthYear}`;
     console.log(output)
 
 
     if(birthYear > 1981 && birthYear < 1996) {
       var millenial = true;
+      //Creating NEW variable with same name as outer scope's variable
+
+      const firstName = 'Steven';
+
+      //Reassigning outer scope's variable
+      output = 'NEW OUTPUT!'
+      
       const str = `Oh, and you are a millenial, ${firstName}`;
       console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+
     }
+
     // console.log(str);
     console.log(millenial);
+    // add(2,3);
   }
   printAge();
 
