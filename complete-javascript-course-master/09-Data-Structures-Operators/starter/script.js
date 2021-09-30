0dc8f7a215fa54e8dac3b5fd9ec1be34edcca13d
AndrewSimons3 <simdrew07@gmail.com>
2021-09-29 21:18:55 -0500

    spread operator prac

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -12,10 +12,6 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  order: function(starterIndex, mainIndex) {
-    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
-  },
-
   openingHours: {
     thu: {
       open: 12,
@@ -30,41 +26,117 @@ const restaurant = {
       close: 24,
     },
   },
+
+  order: function(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
+  },
+
+  orderDelivery: function({starterIndex = 1, mainIndex = 0, address, time = '20:00'}) {
+    console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} 
+    will be delivered to ${address} at ${time}`);
+  }
 };
 
-const arr = [2, 3, 4];
-const a = arr[0];
-const b = arr[1];
-const c = arr[2];
 
-const [x, y, z] = arr;
-console.log(x, y, z);
-console.log(arr);
 
-let [main, , secondary] = restaurant.categories;
-console.log(main, secondary);
+const arr = [7, 8, 9];
+const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+console.log(badNewArr);
 
-// const temp = main;
-// main = secondary;
-// secondary = temp;
+const newArr = [1, 2, ...arr];
+console.log(newArr);
+
+console.log(...newArr);
+console.log(1, 2, 7, 8, 9);
+
+const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+console.log(newMenu);
+
+// Copy array
+const mainMenuCopy = [...restaurant.mainMenu];
+
+//join 2 arrays or more
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(menu);
+
+
+
+
+// //DESTRUCTURING OBJECTS
+// restaurant.orderDelivery({
+//   time: '22:30',
+//   address: 'Via del Sole, 21',
+//   mainIndex: 2,
+//   starterIndex: 2,
+// });
+
+// restaurant.orderDelivery({
+//   address: 'Via del Sole, 21',
+//   starterIndex: 1,
+// })
+
+// const {name, openingHours, categories} = restaurant;
+// console.log(name, openingHours, categories);
+
+// const {
+//   name: restaurantName, 
+//   openingHours: hours, 
+//   categories: tags
+// } = restaurant;
+// console.log(restaurantName, hours, tags);
+
+// //DEFAULT VALUES
+// const {menu = [], starterMenu: starters = []} = restaurant;
+// console.log(menu, starters);
+
+// //MUTATING VARIABLES
+// let a = 111;
+// let b = 989;
+// const obj = { a: 23, b: 7, c:14 };
+
+// ({a, b} = obj);
+// console.log(a, b);
+
+// //NESTED OBJECTS
+// const { fri: {open: o, close: c} } = openingHours;
+// console.log(o, c);
+
+
+
+// //DESTRUCTURING ARRAYS
+// const arr = [2, 3, 4];
+// const a = arr[0];
+// const b = arr[1];
+// const c = arr[2];
+
+// const [x, y, z] = arr;
+// console.log(x, y, z);
+// console.log(arr);
+
+// let [main, , secondary] = restaurant.categories;
 // console.log(main, secondary);
 
-[main, secondary] = [secondary, main];
-console.log(main, secondary);
+// // const temp = main;
+// // main = secondary;
+// // secondary = temp;
+// // console.log(main, secondary);
 
-// Receive 2 return values from a function
-const [starter, mainCourse] = restaurant.order(2, 0);
-console.log(starter, mainCourse);
+// [main, secondary] = [secondary, main];
+// console.log(main, secondary);
 
-//Nested destructuring
-const nested = [2, 4, [5, 6]];
-// const [i, ,j] = nested;
-// console.log(i, j);
+// // Receive 2 return values from a function
+// const [starter, mainCourse] = restaurant.order(2, 0);
+// console.log(starter, mainCourse);
 
-const [i, , [j, k]] = nested;
-console.log(i, j, k)
+// //Nested destructuring
+// const nested = [2, 4, [5, 6]];
+// // const [i, ,j] = nested;
+// // console.log(i, j);
 
-//Default values
-const [p=1, q=1, r=1] = [8, 9];
-console.log(p, q, r);
+// const [i, , [j, k]] = nested;
+// console.log(i, j, k)
+
+// //Default values
+// const [p=1, q=1, r=1] = [8, 9];
+// console.log(p, q, r);
 
