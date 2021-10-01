4ee11c7a82a02ced0104c0e1488faf03a63585ed
AndrewSimons3 <simdrew07@gmail.com>
2021-10-01 13:27:33 -0500

    for of loop

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -46,6 +46,20 @@ const restaurant = {
   }
 };
 
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+for (let item of menu) {
+  console.log(item);
+};
+
+for (let [i, el] of menu.entries()) {
+  
+  console.log(`${i + 1} : ${el}`)
+}
+
+console.log([...menu.entries()])
+
+
 // // restaurant.numGuests = 0;
 // const guests = restaurant.numGuests || 10;
 // console.log(guests);
@@ -301,76 +315,76 @@ game.scored
 GOOD LUCK 😀
 */
 
-const game = {
-  team1: 'Bayern Munich',
-  team2: 'Borrussia Dortmund',
-  players: [
-    [
-      'Neuer',
-      'Pavard',
-      'Martinez',
-      'Alaba',
-      'Davies',
-      'Kimmich',
-      'Goretzka',
-      'Coman',
-      'Muller',
-      'Gnarby',
-      'Lewandowski',
-    ],
-    [
-      'Burki',
-      'Schulz',
-      'Hummels',
-      'Akanji',
-      'Hakimi',
-      'Weigl',
-      'Witsel',
-      'Hazard',
-      'Brandt',
-      'Sancho',
-      'Gotze',
-    ],
-  ],
-  score: '4:0',
-  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
-  date: 'Nov 9th, 2037',
-  odds: {
-    team1: 1.33,
-    x: 3.25,
-    team2: 6.5,
-  },
-};
+// const game = {
+//   team1: 'Bayern Munich',
+//   team2: 'Borrussia Dortmund',
+//   players: [
+//     [
+//       'Neuer',
+//       'Pavard',
+//       'Martinez',
+//       'Alaba',
+//       'Davies',
+//       'Kimmich',
+//       'Goretzka',
+//       'Coman',
+//       'Muller',
+//       'Gnarby',
+//       'Lewandowski',
+//     ],
+//     [
+//       'Burki',
+//       'Schulz',
+//       'Hummels',
+//       'Akanji',
+//       'Hakimi',
+//       'Weigl',
+//       'Witsel',
+//       'Hazard',
+//       'Brandt',
+//       'Sancho',
+//       'Gotze',
+//     ],
+//   ],
+//   score: '4:0',
+//   scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+//   date: 'Nov 9th, 2037',
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+// };
 
 
-//1)
-const [players1, players2] = game.players;
-console.log(players1, players2)
+// //1)
+// const [players1, players2] = game.players;
+// console.log(players1, players2)
 
-//2
-const [gk, ...fieldPlayers] = players1;
-console.log(gk, fieldPlayers)
+// //2
+// const [gk, ...fieldPlayers] = players1;
+// console.log(gk, fieldPlayers)
 
 
-//3
-const allPlayers = [...players1, ...players2];
-console.log(allPlayers)
+// //3
+// const allPlayers = [...players1, ...players2];
+// console.log(allPlayers)
 
-//4
-const newPlayersFinal = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
-console.log(newPlayersFinal)
+// //4
+// const newPlayersFinal = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+// console.log(newPlayersFinal)
 
-//5
-const {odds: {team1, x: draw, team2}} = game;
-console.log(team1, draw, team2)
+// //5
+// const {odds: {team1, x: draw, team2}} = game;
+// console.log(team1, draw, team2)
 
-//6
-const printGoals = function(...players) {
-  console.log(`${players.length} goals were scored`)
-};
+// //6
+// const printGoals = function(...players) {
+//   console.log(`${players.length} goals were scored`)
+// };
 
-printGoals(...game.scored);
+// printGoals(...game.scored);
 
-//7
-team1 < team2 && console.log('Team 1 is more likely to win')
+// //7
+// team1 < team2 && console.log('Team 1 is more likely to win')
 
