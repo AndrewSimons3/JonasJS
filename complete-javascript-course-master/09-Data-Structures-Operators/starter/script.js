765a0a5e3f9686a2031a8a545a0859bfb9c25f32
AndrewSimons3 <simdrew07@gmail.com>
2021-10-01 13:36:52 -0500

    enhanced obj literals

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -1,63 +1,68 @@
 'use strict';
 
 // Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+// const flights =
+//   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+const weekdays = ['mon', 'tues', 'wed', 'thur', 'fri', 'sat', 'sun']
+const hours = {
+  [weekdays[3]]:{
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[5]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
 
-// Data needed for first part of the section
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  
+  //es6 enhanced object literals
+  hours,
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
-
-  order: function(starterIndex, mainIndex) {
+  order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
   },
 
-  orderDelivery: function({starterIndex = 1, mainIndex = 0, address, time = '20:00'}) {
+  orderDelivery({starterIndex = 1, mainIndex = 0, address, time = '20:00'}) {
     console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} 
     will be delivered to ${address} at ${time}`);
   },
 
-  orderPasta: function(ing1, ing2, ing3) {
+  orderPasta(ing1, ing2, ing3) {
     console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, ${ing3}`
     );
   },
 
-  orderPizza: function(mainIngredient, ...otherIngredients) {
+  orderPizza(mainIngredient, ...otherIngredients) {
     console.log(mainIngredient, otherIngredients);
   }
 };
 
-const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 
-for (let item of menu) {
-  console.log(item);
-};
 
-for (let [i, el] of menu.entries()) {
+
+// const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+// for (let item of menu) {
+//   console.log(item);
+// };
+
+// for (let [i, el] of menu.entries()) {
   
-  console.log(`${i + 1} : ${el}`)
-}
+//   console.log(`${i + 1} : ${el}`)
+// }
 
-console.log([...menu.entries()])
+// console.log([...menu.entries()])
 
 
 // // restaurant.numGuests = 0;
