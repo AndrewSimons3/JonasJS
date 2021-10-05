fe9568439c05bb9d2b3e1bcfc4851ffec36e0ebe
AndrewSimons3 <simdrew07@gmail.com>
2021-10-05 08:01:54 -0500

    defualt params finished

--- complete-javascript-course-master/10-Functions/starter/script.js
+++ complete-javascript-course-master/10-Functions/starter/script.js
@@ -2,7 +2,10 @@
 
 const bookings = []
 
-const createBooking = function(flightNum, numPassengers = 1, price = 199) {
+const createBooking = function(
+  flightNum, 
+  numPassengers = 1, 
+  price = 199 * numPassengers) {
   
   // ES5
   // numPassengers = numPassengers || 1;
@@ -19,3 +22,7 @@ const createBooking = function(flightNum, numPassengers = 1, price = 199) {
 
 createBooking('LH123');
 createBooking('LH123', 2, 800);
+createBooking('LH123', 2);
+createBooking('LH123', 5);
+
+createBooking('LH123', undefined, 1000)
