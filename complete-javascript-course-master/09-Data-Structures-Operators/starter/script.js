61eb3d62d47b4218de40cf94385be2bb07253a12
AndrewSimons3 <simdrew07@gmail.com>
2021-10-05 07:51:34 -0500

    working with strings finished

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -1,52 +1,50 @@
 'use strict';
 
-// Data needed for a later exercise
-// const flights =
-//   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
-const weekdays = ['mon', 'tues', 'wed', 'thur', 'fri', 'sat', 'sun']
-const openingHours = {
-  [weekdays[3]]:{
-    open: 12,
-    close: 22,
-  },
-  [weekdays[4]]: {
-    open: 11,
-    close: 23,
-  },
-  [weekdays[5]]: {
-    open: 0, // Open 24 hours
-    close: 24,
-  },
-};
 
-const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+// const weekdays = ['mon', 'tues', 'wed', 'thur', 'fri', 'sat', 'sun']
+// const openingHours = {
+//   [weekdays[3]]:{
+//     open: 12,
+//     close: 22,
+//   },
+//   [weekdays[4]]: {
+//     open: 11,
+//     close: 23,
+//   },
+//   [weekdays[5]]: {
+//     open: 0, // Open 24 hours
+//     close: 24,
+//   },
+// };
+
+// const restaurant = {
+//   name: 'Classico Italiano',
+//   location: 'Via Angelo Tavanti 23, Firenze, Italy',
+//   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+//   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+//   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
   
-  //es6 enhanced object literals
-  openingHours,
+//   //es6 enhanced object literals
+//   openingHours,
 
-  order(starterIndex, mainIndex) {
-    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
-  },
+//   order(starterIndex, mainIndex) {
+//     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
+//   },
 
-  orderDelivery({starterIndex = 1, mainIndex = 0, address, time = '20:00'}) {
-    console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} 
-    will be delivered to ${address} at ${time}`);
-  },
+//   orderDelivery({starterIndex = 1, mainIndex = 0, address, time = '20:00'}) {
+//     console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} 
+//     will be delivered to ${address} at ${time}`);
+//   },
 
-  orderPasta(ing1, ing2, ing3) {
-    console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, ${ing3}`
-    );
-  },
+//   orderPasta(ing1, ing2, ing3) {
+//     console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, ${ing3}`
+//     );
+//   },
 
-  orderPizza(mainIngredient, ...otherIngredients) {
-    console.log(mainIngredient, otherIngredients);
-  }
-};
+//   orderPizza(mainIngredient, ...otherIngredients) {
+//     console.log(mainIngredient, otherIngredients);
+//   }
+// };
 
 
 
@@ -959,19 +957,42 @@ Afterwards, test with your own test data!
 GOOD LUCK 😀
 */
 
-document.body.append(document.createElement('textarea'));
-document.body.append(document.createElement('button'));
+// document.body.append(document.createElement('textarea'));
+// document.body.append(document.createElement('button'));
 
-document.querySelector('button').addEventListener('click', function() {
-  const text = document.querySelector('textarea').value;
-  const rows = text.split('\n');
+// document.querySelector('button').addEventListener('click', function() {
+//   const text = document.querySelector('textarea').value;
+//   const rows = text.split('\n');
 
-  for (const [i, row] of rows.entries()) {
-    const [first, second] = row.toLowerCase().trim().split('_');
-    const output = `${first}${second.replace(
-      second[0], 
-      second[0].toUpperCase()
-    )}`;
-    console.log(`${output.padEnd(20)}${'check'.repeat(i + 1)}`);
-  }
-})
+//   for (const [i, row] of rows.entries()) {
+//     const [first, second] = row.toLowerCase().trim().split('_');
+//     const output = `${first}${second.replace(
+//       second[0], 
+//       second[0].toUpperCase()
+//     )}`;
+//     console.log(`${output.padEnd(20)}${'check'.repeat(i + 1)}`);
+//   }
+// })
+
+
+
+
+// Data needed for a later exercise
+
+// const flights = 
+//   `_Delayed_Departure;fao93766109;txl2133758440;11:25
+//   +_Arrival;bru0943384722;fao93766109;11:45
+//   +_Delayed_Arrival;hel7439299980;fao93766109;12:05
+//   +_Departure;fao93766109;lis2323639855;12:30`;
+
+
+//   const getCode = str => str.slice(0, 3).toUpperCase();
+
+//   for (const flight of flights.split('+')) {
+//     const [type, from, to, time] = flight.split(';');
+//     const output = `${type.startsWith('_Delayed') ? 
+//     '😉' : ''}${type.replaceAll('_', ' ')} 
+//     ${getCode(from)} ${getCode(to)} 
+//     (${time.replace(':', 'h')})`.padStart(36);
+//     console.log(output);
+//   }
