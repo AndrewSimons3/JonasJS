dac34eb196d1d55143a1aed328ca4fae46e7df39
AndrewSimons3 <simdrew07@gmail.com>
2021-10-11 07:16:34 -0500

    sort

--- complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -61,10 +61,13 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = function(movements) {
+const displayMovements = function(movements, sort = false) {
   containerMovements.innerHTML = '';
 
-  movements.forEach((mov, i) => {
+
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+  movs.forEach((mov, i) => {
     const type = mov > 0 ? 'deposit' : 'withdrawal'
 
     const html = `
@@ -194,7 +197,6 @@ btnLoan.addEventListener('click', function(e) {
 });
 
 
-
 btnClose.addEventListener('click', function(e) {
   e.preventDefault();
   
@@ -211,6 +213,13 @@ btnClose.addEventListener('click', function(e) {
       containerApp.style.opacity = 0;
     }
     inputCloseUsername.value = inputClosePin.value = '';
+});
+
+let sorted = false;
+btnSort.addEventListener('click', function(e) {
+  e.preventDefault();
+  displayMovements(currentAccount.movements, !sorted);
+  sorted = !sorted;
 });
 
 
@@ -541,7 +550,7 @@ GOOD LUCK 😀
 //   }
 // }
 
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300]
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300]
 
 
 // // EQUALITY
@@ -580,18 +589,52 @@ console.log(arrDeep.flat(2));
 //   acc + mov, 0);
 //   console.log(overallBalance);
 
-const overallBalance = accounts
-  .map(acc => acc.movements)
-  .flat()
-  .reduce((acc, mov) => acc + mov, 0);
-console.log(overallBalance);
+// const overallBalance = accounts
+//   .map(acc => acc.movements)
+//   .flat()
+//   .reduce((acc, mov) => acc + mov, 0);
+// console.log(overallBalance);
 
 
-const overallBalance2 = accounts
-  .flatMap(acc => acc.movements)
-  .flat()
-  .reduce((acc, mov) => acc + mov, 0);
-console.log(overallBalance);
+// const overallBalance2 = accounts
+//   .flatMap(acc => acc.movements)
+//   .flat()
+//   .reduce((acc, mov) => acc + mov, 0);
+// console.log(overallBalance);
+
+//Strings - mutates array
+const owners = ['Jonas', 'Zach', 'Adam', 'Martha'];
+console.log(owners.sort())
+console.log(owners);
+
+//Numbers
+console.log(movements);
+
+// return < 0, A, B (keep order)
+// return > 0, B, A (switch order)
+
+// Ascending
+movements.sort((a, b) => {
+  if (a > b)
+    return 1;
+  if (a < b)
+    return -1;
+});
+console.log(movements);
+// Ascending
+console.log(movements.sort((a, b) => a - b));
+
+// Descending
+movements.sort((a, b) => {
+  if (a > b) return -1;
+  if (a < b) return 1;
+});
+console.log(movements);
+// Descending
+console.log(movements.sort((a, b) => b - a));
+
+
+
 
 
 
