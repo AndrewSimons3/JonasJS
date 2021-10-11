0418481b7f76a5eb6b60d7d2899945fce2c12560
AndrewSimons3 <simdrew07@gmail.com>
2021-10-10 20:34:44 -0500

    implementing transfer

--- complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -102,11 +102,10 @@ const calcDisplaySummary = function(acc) {
 }
 
 
-const calcDisplayBalance = function(movements) {
-  const balance = movements.reduce((acc, mov) => acc + mov, 0);
-    labelBalance.textContent = `${balance} €`
+const calcDisplayBalance = function(acc) {
+  acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
+    labelBalance.textContent = `${acc.balance}€`
 };
-calcDisplayBalance(account1.movements);
 
 
 
@@ -120,6 +119,17 @@ const createUsernames = function(accs) {
   })
 };
 createUsernames(accounts);
+
+const updateUI = function(acc) {
+   // Display movements
+   displayMovements(acc.movements);
+
+   // Display balance
+   calcDisplayBalance(acc);
+
+   // Display summary
+   calcDisplaySummary(acc);
+}
 
 
 //Event Handlers
@@ -141,19 +151,33 @@ btnLogin.addEventListener('click', function (e) {
     inputLoginUsername.value = inputLoginPin.value = '';
     inputLoginPin.blur();
 
-    // Display movements
-    displayMovements(currentAccount.movements);
-
-    // Display balance
-    calcDisplayBalance(currentAccount.movements);
-
-    // Display summary
-    calcDisplaySummary(currentAccount);
+    //Update UI
+    updateUI(currentAccount);
     
   } 
 });
 
+btnTransfer.addEventListener('click', function(e) {
+  e.preventDefault();
 
+  const amount = Number(inputTransferAmount.value);
+  const receiverAcc = accounts.find(acc => acc.username === inputTransferTo.value);
+  inputTransferAmount.value = inputTransferTo.value = '';
+
+  if(amount > 0 &&
+    receiverAcc &&
+    currentAccount.balance >= amount && 
+    receiverAcc?.username !== currentAccount.username) {
+
+      // Doing the transfer
+      currentAccount.movements.push(-amount);
+      receiverAcc.movements.push(amount);
+
+      //update UI
+      updateUI(currentAccount);
+
+    }
+});
 
 
 
