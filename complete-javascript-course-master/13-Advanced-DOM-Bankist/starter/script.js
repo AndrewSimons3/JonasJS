f9ee5e6788c2d199960c29bf23d538206cd7b381
AndrewSimons3 <simdrew07@gmail.com>
2021-10-16 08:50:54 -0500

    capturing propogation, bubbling and capturing

--- complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
@@ -155,9 +155,45 @@ document.addEventListener('keydown', function (e) {
 // });
 
 
-const h1 = document.querySelector('h1');
+// const h1 = document.querySelector('h1');
 
-h1.addEventListener('mouseenter', function(e) {
-  alert('addEventListener: Great! You are reading the heading')
+// const alertH1 = function (e) {
+//   alert('addEventListener: Great! You are reading the heading :D');
+
+// };
+
+// h1.addEventListener('mouseenter', alertH1);
+
+// setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
+
+// // h1.onmouseenter = function(e) {
+// //   alert('addEventListener: Great! You are reading the heading')
+// // };
+
+// rgb(255, 255, 255)
+const randomInt = (min, max) => 
+  Math.floor(Math.random() * (max-min + 1) + min);
+
+const randomColor = () => 
+  `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
+
+ document.querySelector('.nav__link').addEventListener('click', function(e) {
+   this.style.backgroundColor = randomColor();
+   console.log('LINK', e.target, e.currentTarget);
+
+   // Stop propagation
+  //  e.stopPropagation();
+ });
+
+ document.querySelector('.nav__links').addEventListener('click', function(e) {
+  this.style.backgroundColor = randomColor();
+  console.log('CONTAINER', e.target, e.currentTarget);
 });
+
+document.querySelector('.nav').addEventListener('click', function(e) {
+  this.style.backgroundColor = randomColor();
+  console.log('NAV', e.target, e.currentTarget);
+});
+
+
 
