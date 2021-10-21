76373597269afaa3c3dc7ad77b1d76028e93476d
AndrewSimons3 <simdrew07@gmail.com>
2021-10-21 06:36:24 -0500

    getters and setters

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -92,22 +92,93 @@ DATA CAR 2: 'Mercedes' going at 95 km/h
 GOOD LUCK 😀
 */
 
-const Car = function(make, speed) {
-  this.make = make;
-  this.speed = speed;
-}
+// const Car = function(make, speed) {
+//   this.make = make;
+//   this.speed = speed;
+// }
 
-Car.prototype.accelerate = function () {
-    console.log(this.speed + 10);
-  };
+// Car.prototype.accelerate = function () {
+//     console.log(this.speed + 10);
+//   };
 
-Car.prototype.brake = function() {
-  console.log(this.speed - 10);
-}
+// Car.prototype.brake = function() {
+//   console.log(this.speed - 10);
+// }
 
-const car1 = new Car("BMW", 120);
-const car2 = new Car("Mercedes", 95);
+// const car1 = new Car("BMW", 120);
+// const car2 = new Car("Mercedes", 95);
 
-car1.accelerate();
-car2.brake();
+// car1.accelerate();
+// car2.brake();
+
+//class expression
+// const PersonCl = class {}
+
+//declaration
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
   
+  // Methods will be added to .prototype property
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.firstName}`);
+  }
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  // Set a property that already exists
+  set fullName(name) {
+    console.log(name);
+    if(name.includes('')) this._fullName = name;
+    else alert(`${name} is not a full name!`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+}
+
+const jessica = new PersonCl('Jessica Davis', 1996);
+console.log(jessica);
+jessica.calcAge();
+console.log(jessica.age);
+
+console.log(jessica.__proto__ === PersonCl.prototype);
+  
+// PersonCl.prototype.greet = function() {
+//   console.log(`Hey ${this.firstName}`);
+// };
+
+jessica.greet();
+
+// 1. Classes are NOT hoisted
+// 2. Classes are first-class citizens
+// 3. Classes are executed in strict mode
+
+const walter = new PersonCl('Walter White', 1965);
+
+const account = {
+  owner: 'jonas',
+  movements: [200, 530, 120, 300],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  }
+};
+
+console.log(account.latest);
+
+account.latest = 50;
+console.log(account.movements);
