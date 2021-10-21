bf72d1cb452c5d17df7ad56cbd5e2c2cfeec9d4e
AndrewSimons3 <simdrew07@gmail.com>
2021-10-21 07:52:48 -0500

    complete

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -324,39 +324,39 @@ DATA CAR 1: 'Tesla' going at 120 km/h, with a charge of 23%
 GOOD LUCK 😀
 */
 
-// const Car = function(make, speed) {
-//   this.make = make;
-//   this.speed = speed;
-// }
+const Car = function(make, speed) {
+  this.make = make;
+  this.speed = speed;
+}
 
-// Car.prototype.accelerate = function () {
-//     console.log(this.speed + 10);
-//   };
+Car.prototype.accelerate = function () {
+    console.log(this.speed + 10);
+  };
 
-// Car.prototype.brake = function() {
-//   console.log(this.speed - 10);
-// }
+Car.prototype.brake = function() {
+  console.log(this.speed - 10);
+}
 
-// const EV = function(make, speed, charge) {
-//   Car.call(this, make, speed);
-//   this.charge = charge;
-// }
+const EV = function(make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+}
 
-// EV.prototype = Object.create(Car.prototype)
+EV.prototype = Object.create(Car.prototype)
 
-// EV.prototype.chargeBattery = function(chargeTo) {
-//   this.charge = chargeTo;
-// };
+EV.prototype.chargeBattery = function(chargeTo) {
+  this.charge = chargeTo;
+};
 
-// EV.prototype.accelerate = function() {
-//   this.speed += 20;
-//   this.charge--;
-//   console.log(`${this.make} is going at ${this.speed} km/h, with a charge of ${this.charge}`)
-// }
+EV.prototype.accelerate = function() {
+  this.speed += 20;
+  this.charge--;
+  console.log(`${this.make} is going at ${this.speed} km/h, with a charge of ${this.charge}`)
+}
 
-// const tesla = new EV('Tesla', 120, 23);
-// tesla.chargeBattery(90);
+const tesla = new EV('Tesla', 120, 23);
+tesla.chargeBattery(90);
 
-// console.log(tesla)
-// tesla.brake()
-// tesla.accelerate();
+console.log(tesla)
+tesla.brake()
+tesla.accelerate();
