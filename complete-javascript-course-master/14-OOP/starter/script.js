2db5680e5b72692d3d13a0291822d4c29c065f4a
AndrewSimons3 <simdrew07@gmail.com>
2021-10-22 06:49:34 -0500

    private class fields and methods

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -462,40 +462,64 @@ GOOD LUCK 😀
 // jay.calcAge();
 
 
+// 1) Public fields
+// 2) Private fields
+// 3) Public Methods
+// 4) Private Methods
+// (there is also the static version)
+
+
 class Account {
+  // 1) Public fields (instances)
+  locale = navigator.language;
+  
+  // 2) Private fields (instances)
+  #movements = [];
+  #pin;
+
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currency = currency;
     // protected property
-    this._pin = pin;
-    this._movements = [];
-    this.locale = navigator.language;
+    this.#pin = pin;
+    // this._movements = [];
+    // this.locale = navigator.language;
 
     console.log(`Thanks for opening an account, ${owner}`)
   }
 
+  // Public Methods
+
   // Public interface
   getMovements() {
-    return this._movements;
+    return this.#movements;
   }
 
   deposit(val) {
-    this._movements.push(val)
+    this.#movements.push(val)
   }
 
   withdraw(val) {
     this.deposit(-val)
   }
 
-  _approveLoan(val) {
-    return true;
-  }
+ 
 
   requestLoan(val) {
     if(this._approveLoan(val)) {
       this.deposit(val);
       console.log(`Loan approved`)
     }
+  }
+
+  static helper() {
+    console.log('Helper');
+  }
+
+  // 4) Private methods
+  // #approveLoan(val) {
+  _approveLoan() {
+    return true;
   }
 }
 
@@ -512,3 +536,9 @@ console.log(acc1.getMovements());
 
 console.log(acc1);
 // console.log(acc1.pin);
+
+// console.log(acc1.#movements)
+// console.log(acc1.#pin)
+// console.log(ac1.#approveLoan(100));
+
+Account.helper();
