ef3c634fb0b55c3a621d45e86c10547da51cf95e
AndrewSimons3 <simdrew07@gmail.com>
2021-11-05 06:15:45 -0500

    event loop prac

--- complete-javascript-course-master/16-Asynchronous/starter/script.js
+++ complete-javascript-course-master/16-Asynchronous/starter/script.js
@@ -272,4 +272,10 @@ setTimeout(() => console.log('0 sec timer'), 0);
 
 Promise.resolve('Resolved promise 1').then(res =>
   console.log(res));
-  console.log('Test end')
+
+Promise.resolve('Resolved promise 2').then(res => {
+  for (let i = 0; i < 100000000; i++) {}
+  console.log(res);
+});
+
+console.log('Test end')
