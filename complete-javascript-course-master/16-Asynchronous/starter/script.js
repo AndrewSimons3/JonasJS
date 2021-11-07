803ba061b10df0c9f75ae2039d0c3864b5aafb62
AndrewSimons3 <simdrew07@gmail.com>
2021-11-07 08:10:21 -0600

    promise.all

--- complete-javascript-course-master/16-Asynchronous/starter/script.js
+++ complete-javascript-course-master/16-Asynchronous/starter/script.js
@@ -489,13 +489,34 @@ const whereAmI = async function () {
   }
 };
 
-
-console.log('1: Will get location');
+// console.log('1: Will get location');
 // const city = whereAmI();
 // console.log(city);
 
-whereAmI()
-  .then(city => console.log(`2: ${city}`))
-  .catch(err => console.error(`${err.message} 💥`))
-  .finally(() => console.log('3: Finished getting location'));
+// (async function() {
+//   try {
+//     const city = await whereAmI();
+//     console.log(`2: ${city}`)
+//   } catch(err) {
+//     console.error(`${err.message} 💥`)
+//   }
+//   console.log('3: Finished getting location');
+// })()
 
+const get3Countries = async function (c1, c2, c3) {
+  try {
+    // const [data1] = await getJSON(`https://restcountries.com/v2/name/${c1}`);
+    // const [data2] = await getJSON(`https://restcountries.com/v2/name/${c2}`);
+    // const [data3] = await getJSON(`https://restcountries.com/v2/name/${c3}`);
+
+    const data = await Promise.all([
+      getJSON(`https://restcountries.com/v2/name/${c1}`),
+      getJSON(`https://restcountries.com/v2/name/${c2}`),
+      getJSON(`https://restcountries.com/v2/name/${c3}`),
+    ]);
+    console.log(data.map(d => d[0].capital));
+  } catch (err) {
+    console.log(err);
+  }
+};
+get3Countries('portugal', 'canada', 'tanzania');
