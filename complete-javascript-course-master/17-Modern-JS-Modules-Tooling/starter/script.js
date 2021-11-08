a32dac918e5db0d198ad4dfe1288ddafbc00f5aa
AndrewSimons3 <simdrew07@gmail.com>
2021-11-08 06:56:43 -0600

    module pattern

--- complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
+++ complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
@@ -2,19 +2,57 @@
 //  import { addToCart, tq, totalPrice as price } from './shoppingCart.js';
 // addToCart('bread', 5);
 // console.log(price, tq);
-console.log('Importing module');
+// console.log('Importing module');
 
-//  import * as ShoppingCart from './shoppingCart.js';
-//  ShoppingCart.addToCart('bread', 5);
-//  console.log(ShoppingCart.totalPrice);
+// //  import * as ShoppingCart from './shoppingCart.js';
+// //  ShoppingCart.addToCart('bread', 5);
+// //  console.log(ShoppingCart.totalPrice);
 
- import add, {cart} from './shoppingCart.js';
- add('pizza', 2);
- add('bread', 5);
- add('apple', 4);
+//  import add, {cart} from './shoppingCart.js';
+//  add('pizza', 2);
+//  add('bread', 5);
+//  add('apple', 4);
 
- console.log(cart);
+//  console.log(cart);
 
+
+// const ShoppingCart2 = (function() {
+//   const cart = [];
+//   const shippingCost = 10;
+//   const totalPrice = 237;
+//   const totalQuantity = 23;
+
+//   const addToCart = function(product, quantity) {
+//     cart.push({product, quantity});
+//     console.log(`${quantity} ${product} added to cart`);
+//   };
+
+//   const orderStock = function(product, quantity) {
+//     console.log(`${quantity} ${product} ordered from supplier`);
+//   };
+
+//   return {
+//     addToCart,
+//     cart,
+//     totalPrice,
+//     totalQuantity,
+//   };
+// })();
+
+// ShoppingCart2.addToCart('apple', 4);
+// ShoppingCart2.addToCart('pizza', 2);
+// console.log(ShoppingCart2);
+
+
+// Export
+// export.addToCart = function(product, quantity) {
+//       cart.push({ product, quantity });
+//       console.log(`${quantity} ${product} added to cart (shipping cost is ${shippingCost})`
+//       );
+//     };
+
+// // Import
+// const { addToCart } = require('./shoppingCart.js')
 
  
 
